76bb929476e27304748ccaca23ab9d0755ba6ffb
Taya Mika <cillatx30@gmail.com>
2015-08-14 11:34:28 -0500

    Add 'about' action to Static Pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+
 end
