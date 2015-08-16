9f16a34209080b9d815feae8e052d4e735686ea6
Taya Mika <cillatx30@gmail.com>
2015-08-16 03:18:52 -0500

    Generate a Users controller with a 'new' action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 end
