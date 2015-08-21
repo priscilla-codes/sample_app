4cca6b932a5c2cac85cde77409035210d279cb38
Taya Mika <cillatx30@gmail.com>
2015-08-20 20:27:34 -0500

    Add Log in/ Log out functionality and 'forget' users on browser close

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
